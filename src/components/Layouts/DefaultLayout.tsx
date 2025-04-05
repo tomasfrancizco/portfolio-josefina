4fceafd7321246f766efa1ba9b2db26d4c1539e9
Tomas <tomasfreire@gmail.com>
2025-04-05 20:12:01 -0300

    first commit

---
+++ src/components/Layouts/DefaultLayout.tsx
@@ -0,0 +1,35 @@
+"use client";
+import React, { ReactNode } from "react";
+import Navbar from "@/components/Navbar";
+
+interface DefaultLayoutProps {
+  children: ReactNode;
+}
+
+export default function DefaultLayout({
+  children,
+}: DefaultLayoutProps) {
+  return (
+    <>
+      {/* <!-- ===== Page Wrapper Start ===== --> */}
+      <div className="flex h-screen overflow-hidden">
+        {/* <!-- ===== Content Area Start ===== --> */}
+        <div className="relative flex flex-1 flex-col overflow-y-auto overflow-x-hidden">
+          {/* <!-- ===== Navbar Start ===== --> */}
+          <Navbar sidebarOpen={false} setSidebarOpen={() => {}} />
+          {/* <!-- ===== Navbar End ===== --> */}
+
+          {/* <!-- ===== Main Content Start ===== --> */}
+          <main>
+            <div className="mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10">
+              {children}
+            </div>
+          </main>
+          {/* <!-- ===== Main Content End ===== --> */}
+        </div>
+        {/* <!-- ===== Content Area End ===== --> */}
+      </div>
+      {/* <!-- ===== Page Wrapper End ===== --> */}
+    </>
+  );
+}
